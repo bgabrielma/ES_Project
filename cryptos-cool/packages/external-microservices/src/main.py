39adb06c3fbe3b00a30cf6061c67073709d7efbf
Bruno Martins <brunonunes.martins@my.istec.pt>
2023-01-13 19:35:44 +0000

    feat: created news service api for news cryptoscool project portal consumption

--- cryptos-cool/packages/external-microservices/src/main.py
+++ cryptos-cool/packages/external-microservices/src/main.py
@@ -1,17 +1,25 @@
-import os
-import json
+import requests
 
-from flask import Flask
+from flask import Flask, request as flask_request
 from dotenv import load_dotenv
+from utils import build_url
 
 load_dotenv()
 
 app = Flask("news")
 
-@app.route("/", methods=['GET'])
-def hello_world():
-    return json.dumps({'name': 'alice',
-                       'email': 'alice@outlook.com'})
+@app.route("/news", methods=['GET'])
+def get_news():
+    """
+    Args: 
+        search -> Ex: crypto AND bitcoin
+        page -> Ex: 1
+        from -> Ex> 2023-01-01. Must be in YYYY-MM-DD
+        to -> Ex> 2023-01-01. Must be in YYYY-MM-DD
+    """
+    url = build_url(flask_request.args)
+    print(url)
+    return requests.get(url).json()
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000, debug=True)
