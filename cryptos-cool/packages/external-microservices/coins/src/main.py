9d4ee38a364dbe72d7e1c6982a163f311b408c88
pedro ramos <pedro.ramos9@hotmail.com>
2023-01-19 23:51:36 +0000

    changed the way we send data

---
+++ cryptos-cool/packages/external-microservices/coins/src/main.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import datetime
+
+from flask import Flask, jsonify
+from dotenv import load_dotenv
+
+from utils import get_data
+from constants import FULL_DAY_SECONDS
+
+load_dotenv()
+
+app = Flask("coins")
+
+@app.route("/coins/<string:type>/<int:days>", methods=['GET'])
+def get_coins(type, days):
+    """
+    Args: 
+    type = type of crypto coin
+    days = int number of days. Maximum 14
+    """
+    data = get_data(type, days)
+    
+    return jsonify({
+        "from": int(datetime.datetime.now().timestamp() - (days * FULL_DAY_SECONDS)),
+        "to": int(datetime.datetime.now().timestamp()),
+        "coin": type,
+        "data" : data
+    })
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=1000, debug=True)
