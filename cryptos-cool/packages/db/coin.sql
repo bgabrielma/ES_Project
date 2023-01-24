1f266437f94794d0e56f72eae3a70a681d0d9f5d
pedroplays2020 <102863935+pedroplays2020@users.noreply.github.com>
2023-01-24 23:22:49 +0000

    database updated

---
+++ cryptos-cool/packages/db/coin.sql
@@ -0,0 +1,14 @@
+create database coins;
+use coins;
+ create table coin(
+ name varchar(100) not null ,
+ acronyum varchar(100 ) not null,
+ lowestValue int not null ,
+ highestValue int not null,
+ currentValue int not null,
+ createdAt TIMESTAMP,
+ updatedAt TIMESTAMP 
+ );
+
+ 
+ 
